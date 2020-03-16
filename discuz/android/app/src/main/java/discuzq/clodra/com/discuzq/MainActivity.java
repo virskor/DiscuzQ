c3b22bb4b05d3841833efca6c22dcc591e5b93de
virskor <virskor>
2020-03-16 16:21:56 +0800

    新增splash screen 一键生成

--- discuz/android/app/src/main/java/discuzq/clodra/com/discuzq/MainActivity.java
+++ discuz/android/app/src/main/java/discuzq/clodra/com/discuzq/MainActivity.java
@@ -5,6 +5,9 @@ import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
+import android.os.Build;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 public class MainActivity extends FlutterActivity {
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
---
+++ discuz/splash
@@ -0,0 +1,1 @@
+flutter pub pub run flutter_native_splash:create
