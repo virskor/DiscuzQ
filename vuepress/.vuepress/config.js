6942538b8d77e61dd378d562dff7d76fcadfbaca
virskor <virskor>
2020-04-14 13:11:42 +0800

    增加docs

--- vuepress/.vuepress/config.js
+++ vuepress/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     dest: '../docs',
     /// base 值应当总是以斜杠开始，并以斜杠结束
-    base: '/docs/',
+    base: '/DiscuzQ/docs/',
     ///
     title:'DiscuzQ Mobile App Doc',
     description: 'DiscuzQ App document for building your application',
