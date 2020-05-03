73fbfba75edc8ed390bf7c0f619d7248639c8651
virskor <virskor>
2020-05-03 23:25:08 +0800

    优化付费内容提示

--- discuz/lib/widgets/threads/payments/threadRequiredPayments.dart
+++ discuz/lib/widgets/threads/payments/threadRequiredPayments.dart
@@ -1,7 +1,12 @@
 import 'package:discuzq/models/threadModel.dart';
+import 'package:discuzq/utils/global.dart';
+import 'package:discuzq/widgets/common/discuzButton.dart';
+import 'package:discuzq/widgets/common/discuzIcon.dart';
 import 'package:discuzq/widgets/common/discuzText.dart';
 import 'package:discuzq/widgets/ui/ui.dart';
+import 'package:discuzq/widgets/webview/webviewHelper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sfsymbols/flutter_sfsymbols.dart';
 
 class ThreadRequiredPayments extends StatelessWidget {
   ///
@@ -13,14 +18,14 @@ class ThreadRequiredPayments extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (thread.attributes.canViewPosts &&
+    if (thread.attributes.paid ||
         double.tryParse(thread.attributes.price) == 0) {
       return const SizedBox();
     }
 
     return Container(
       margin: const EdgeInsets.only(top: 10),
-      padding: const EdgeInsets.all(5),
+      padding: const EdgeInsets.all(15),
       decoration: BoxDecoration(
           borderRadius: const BorderRadius.all(Radius.circular(10)),
           color: DiscuzApp.themeOf(context).scaffoldBackgroundColor),
@@ -33,6 +38,19 @@ class ThreadRequiredPayments extends StatelessWidget {
               '包含受保护的内容，暂时无权限查看',
               fontWeight: FontWeight.bold,
             ),
+          ),
+          const SizedBox(height: 10),
+          DiscuzButton(
+            label: '了解详情',
+            width: 140,
+            height: 35,
+            icon: const DiscuzIcon(
+              SFSymbols.doc_text_search,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              WebviewHelper.launchUrl(url: '${Global.domain}/details/${thread.id.toString()}');
+            },
           )
         ],
       ),
